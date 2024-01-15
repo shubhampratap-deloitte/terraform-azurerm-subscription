7236632e00aa554c1df287aae0d3ea35a43f422a
shubhampratap-deloitte <shupundir@deloitte.com>
2024-01-15 23:35:25 +0530

    changes in main.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.86.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+   features {}
+#   client_id = var.client_id
+#   client_secret = var.client_secret
+#   tenant_id = var.tenant_id
+#   subscription_id = var.subscription_id
+}
