4b133b4785376029844b560e4c191153d9d2d8ca
anshagrawal2_deloitte <anshagrawal2@deloitte.com>
2024-01-16 16:32:29 +0530

    added untested subscription module

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 data "azurerm_billing_enrollment_account_scope" "subscription" {
-  billing_account_name    = "1234567890"
-  enrollment_account_name = "0123456"
+  billing_account_name    = var.billing_account_name
+  enrollment_account_name = var.enrollment_account_name
 }
 
-resource "azurerm_subscription" "test" {
-  subscription_name = "My Example EA Subscription"
+resource "azurerm_subscription" "subscription_create" {
+  subscription_name = var.subscription_name
   billing_scope_id  = data.azurerm_billing_enrollment_account_scope.subscription.id
 }
 
 resource "azurerm_management_group_subscription_association" "subscription_associate" {
   management_group_id =  "/providers/Microsoft.Management/managementGroups/${var.subscription_management_group_id_name}"
-#  subscription_id     =  "/subscriptions/${var.associate_subscriptions_id_name}"
-  subscription_id     =  "/subscriptions/${azurerm_subscription.test.id}"
+   subscription_id     =  "/subscriptions/${azurerm_subscription.subscription_create.id}"
 }
