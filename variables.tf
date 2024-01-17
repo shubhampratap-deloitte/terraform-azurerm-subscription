1c21e9326ed2d9027f2a8099b526093eefc46294
anshagrawal2_deloitte <anshagrawal2@deloitte.com>
2024-01-17 17:37:15 +0530

    added files

--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "subscription_management_group_id_name" {
   type        = string
-  description = "Id of the Management Group You want the Subscription to be Associated"
+  description = "(Required) The ID of the Management Group to associate the Subscription with. *Changing this forces a new Management to be created.*"
 
 }
 variable "subscription_name" {
-  type = string
+  type        = string
+  description = "(Required) The Name of the Subscription. This is the Display Name in the portal."
 }
 variable "enrollment_account_name" {
   type        = string
@@ -13,27 +14,30 @@ variable "enrollment_account_name" {
 }
 variable "billing_account_name" {
   type        = string
-  description = "Detail of the Billing Account Name"
+  description = "(Optional) The Azure Billing Scope ID. Can be a Microsoft Customer Account Billing Scope ID, a Microsoft Partner Account Billing Scope ID or an Enrollment Billing Scope ID."
 
 }
-
 variable "client_id" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "Azure Client ID (Application Object ID)"
 }
 
 variable "client_secret" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "The Client Secret for the Client ID"
 }
 
 variable "tenant_id" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = " The Azure Tenant ID."
 }
 
 variable "subscription_id" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "The Azure Subscription ID."
 }
 
