12bd25b2f12ac52e68710c9f3c41ecf4ac43ac46
anshagrawal2_deloitte <anshagrawal2@deloitte.com>
2024-01-18 13:16:46 +0530

    untested and sanitized

--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
 }
