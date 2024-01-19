c29edc8bb131da0cfa46cea6781e1e0b35678633
anshagrawal2_deloitte <anshagrawal2@deloitte.com>
2024-01-19 13:42:30 +0530

    update module

--- output.tf
+++ output.tf
@@ -1,12 +1,9 @@
-output "billing_enrollment_account_scope_id" {
-  description = "ID of the billing enrollment account scope."
-  value       = data.azurerm_billing_enrollment_account_scope.subscription.id
+output "subscription_details" {
+  value = { for s in azurerm_subscription.subscription_create : s.name => s }
+  description = "The details of the created subscriptions"
 }
-output "created_subscription_id" {
-  description = "ID of the newly created Azure subscription."
-  value       = azurerm_subscription.subscription_create.id
-}
-output "management_group_subscription_association_id" {
-  description = "ID of the management group subscription association."
-  value       = azurerm_management_group_subscription_association.subscription_associate.id
+
+output "subscription_association_details" {
+  value = { for a in azurerm_management_group_subscription_association.subscription_associate : a.subscription_id => a }
+  description = "The details of the subscription associations with management groups"
 }
